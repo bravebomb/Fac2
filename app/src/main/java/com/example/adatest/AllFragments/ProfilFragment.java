e1f5e4f507a3c586a18a3d532abf2c5f15baf41d
Lokkrip <arvidblomberg13@gmail.com>
2022-10-12 10:11:44 +0200

    remember user, logout, sharedpref för namn

--- app/src/main/java/com/example/adatest/AllFragments/ProfilFragment.java
+++ app/src/main/java/com/example/adatest/AllFragments/ProfilFragment.java
@@ -1,6 +1,8 @@
 package com.example.adatest.AllFragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,12 +15,14 @@ import android.widget.TextView;
 
 import com.example.adatest.CoopActivity;
 import com.example.adatest.FAQActivity;
+import com.example.adatest.MainActivity;
 import com.example.adatest.R;
 
 public class ProfilFragment extends Fragment {
 
     private TextView FAQButton;
-
+    private TextView logOutButton;
+    private TextView namn;
 
     public ProfilFragment() {
         // Required empty public constructor
@@ -30,10 +34,29 @@ public class ProfilFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profil, container, false);
         FAQButton = view.findViewById(R.id.register_button_FAQ);
+        logOutButton = view.findViewById(R.id.logout);
+        namn = view.findViewById(R.id.profile_name);
+
+        SharedPreferences preferences = getActivity().getSharedPreferences("loginNamePref", Context.MODE_PRIVATE);
+        String userName = (preferences.getString("userNameKey", ""));
+        namn.setText(userName);
+
         FAQButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent in = new Intent(getActivity(), FAQActivity.class);
+                startActivity(in);
+            }
+        });
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences = getActivity().getSharedPreferences("checkbox", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("remember", "false");
+                editor.apply();
+                Intent in = new Intent(getActivity(), MainActivity.class);
                 startActivity(in);
             }
         });
--- app/src/main/java/com/example/adatest/MainActivity.java
+++ app/src/main/java/com/example/adatest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.adatest;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,7 +15,10 @@ import android.widget.Toast;
 public class MainActivity extends userInfoAppActivity {
 
     EditText loginName, loginPass;
+    CheckBox checkBox;
+    Boolean isCheckboxChecked = false;
     String url = "https://hex.cse.kau.se/~arviblom100/getinfo.php";
+    SharedPreferences loginNamePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,22 @@ public class MainActivity extends userInfoAppActivity {
         TextView registerButton = findViewById(R.id.register_button);
         loginName = findViewById(R.id.loginName);
         loginPass = findViewById(R.id.loginPass);
+        checkBox = findViewById(R.id.checkbox);
         TextView title = findViewById(R.id.titleId);
+
+
+        loginNamePref = getSharedPreferences("loginNamePref", MODE_PRIVATE);
+
+        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        String checkbox = preferences.getString("remember", "");
+
+
+        if(checkbox.equals("true")){
+            Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+            startActivity(intent);
+        }else if(checkbox.equals(false)){
+
+        }
 
         enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,15 +52,39 @@ public class MainActivity extends userInfoAppActivity {
                 doesUserPassExistInDatabase(new VolleyCallBack() {
                     @Override
                     public void onSuccess() {
+                        if(isCheckboxChecked){
+                            SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            editor.putString("remember", "true");
+                            editor.apply();
+                        }
+                        SharedPreferences.Editor loginEditor = loginNamePref.edit();
+                        loginEditor.putString("userNameKey", loginName.toString());
+                        loginEditor.apply();
                         Intent intent = new Intent(MainActivity.this, FirstActivity.class);
                         startActivity(intent);
                     }
                     @Override
                     public void onFailure() {
+                        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("remember", "false");
+                        editor.apply();
                         Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
                     }
                 }, loginName.getText().toString(), loginPass.getText().toString(), MainActivity.this);
 
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    isCheckboxChecked = true;
+                }else if(!compoundButton.isChecked()){
+                    isCheckboxChecked = false;
+                }
             }
         });
 
